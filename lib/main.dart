9e859e600375f470bd4dee3ed78d90e82d8223ed
Vinayaki Dalvi <80617080+VinayakiDalvi@users.noreply.github.com>
2023-03-25 14:16:43 +0530

    add record screen added

--- lib/main.dart
+++ lib/main.dart
@@ -21,7 +21,7 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.grey,
       ),
       debugShowCheckedModeBanner: false,
-      initialRoute: AppRoutes.searchrecord,
+      initialRoute: AppRoutes.initialRoute,
       getPages: AppRoutes.pages,
     );
   }
--- lib/screens/add_record_screen/controller/add_record_screen_controller.dart
+++ lib/screens/add_record_screen/controller/add_record_screen_controller.dart
@@ -3,15 +3,40 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 import '../../../core/constants/firebaseconstants.dart';
+import '../../../data/models/selection_pop_up_model/selction_pop_up_model.dart';
+import '../models/add_new_record_model.dart';
 
 class AddRecordPageController extends GetxController {
+  TextEditingController nameController = TextEditingController();
+  TextEditingController ageController = TextEditingController();
+  TextEditingController numberController = TextEditingController();
+  TextEditingController addressController = TextEditingController();
+  TextEditingController weightController = TextEditingController();
+  TextEditingController temperatureController = TextEditingController();
+  TextEditingController bpController = TextEditingController();
+  TextEditingController prescriptionController = TextEditingController();
+  SelectionPopupModel? selectedDropDownValue;
+  late List<String> symptoms;
+  Rx<Add_New_Record_Model> add_new_record_ModelObj = Add_New_Record_Model().obs;
   @override
   void onReady() {
     super.onReady();
+
   }
 
   @override
   void onClose() {
     super.onClose();
+  }
+
+  onSelected(dynamic value) {
+    selectedDropDownValue = value as SelectionPopupModel;
+    add_new_record_ModelObj.value.dropdownItemList.forEach((element) {
+      element.isSelected = false;
+      if (element.id == value.id) {
+        element.isSelected = true;
+      }
+    });
+    add_new_record_ModelObj.value.dropdownItemList.refresh();
   }
 }
